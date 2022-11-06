83913343a7e130b0ff3efce211e4adc6cb0c6953
to-voando <miguelcoruj@gmail.com>
2022-11-06 20:40:53 -0300

    utilities

---
+++ src/utils/Colors.java
@@ -0,0 +1,19 @@
+package utils;
+
+import java.awt.Color;
+
+public class Colors {
+    public final static Color[] RANDOM = {
+        // BLUE
+        new Color(42, 78, 112),
+        new Color(7, 121, 242),
+
+        // PURPLE
+        new Color(117, 7, 242),
+        // PINK
+        new Color(196, 114, 158),
+
+        // GREEN
+        new Color(81, 150, 12),
+    };
+}
---
+++ src/utils/GenerateJson.java
@@ -0,0 +1,5 @@
+package utils;
+
+public interface GenerateJson {
+    public String toJson();
+}
