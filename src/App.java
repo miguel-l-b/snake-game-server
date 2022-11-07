0853e6f09c4ccacfd46f358565a041c8a1234806
Bueno <113569357+HenriqueCBueno@users.noreply.github.com>
2022-11-06 22:57:04 -0300

    stylized
    Co-Authored-By: V3dr0n1 <116525416+V3dr0n1@users.noreply.github.com>
    Co-Authored-By: Miguel Lopes Braido <59874694+miguel-l-b@users.noreply.github.com>

--- src/App.java
+++ src/App.java
@@ -5,10 +5,8 @@ import Input.Keyboard;
 public class App {
     public static void main(String[] args) {
         ConsoleManager.print(Colors.CYAN,"Port: ");
-        ConsoleManager.setColor(Colors.BLACK, Colors.WHITE_BACKGROUND);
         try {
             int port = Keyboard.getInt();
-            ConsoleManager.resetColor();
             
             new Server(port);
             ConsoleManager.println(Colors.GREEN, "Started in "+port);
