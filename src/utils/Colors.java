c92efeb75b845422b941149507d05ee3a825d050
Bueno <113569357+HenriqueCBueno@users.noreply.github.com>
2022-11-06 23:00:34 -0300

    fix
    Co-Authored-By: V3dr0n1 <116525416+V3dr0n1@users.noreply.github.com>
    Co-Authored-By: Miguel Lopes Braido <59874694+miguel-l-b@users.noreply.github.com>

--- src/utils/Colors.java
+++ src/utils/Colors.java
@@ -1,9 +1,10 @@
 package utils;
 
 import java.awt.Color;
+import java.util.Random;
 
-public class Colors {
-    public final static Color[] RANDOM = {
+public class ColorsPlayer {
+    public static Color[] COLORS = {
         // BLUE
         new Color(42, 78, 112),
         new Color(7, 121, 242),
@@ -16,4 +17,11 @@ public class Colors {
         // GREEN
         new Color(81, 150, 12),
     };
+
+    public static Color RANDOM() {
+        // Random random = new Random();
+        // int color = random.nextInt(COLORS.length);
+        // return COLORS[color];
+        return COLORS[new Random().nextInt(COLORS.length)];
+    }
 }
