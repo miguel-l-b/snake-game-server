b3467f92627299831387a2340dbfb5b449088d7e
Miguel Lopes Braido <miguelcoruj@gmail.com>
2022-11-10 18:19:35 -0300

    Communicate login of cliente
    Co-Authored-By: V3dr0n1 <116525416+V3dr0n1@users.noreply.github.com>

---
+++ src/controller/LogIn.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class LogIn extends Communicate {
+    public final String USERNAME;
+
+    public LogIn(String username) {
+        this.USERNAME = username;
+    }
+}
