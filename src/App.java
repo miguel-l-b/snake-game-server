1c85fb7dfd1a9aeabe022efc4f295ef032ff7b33
to-voando <miguelcoruj@gmail.com>
2022-11-14 11:04:51 -0300

    kill server

--- src/App.java
+++ src/App.java
@@ -1,15 +1,25 @@
+import java.net.ServerSocket;
+
 import Console.Colors;
 import Console.ConsoleManager;
 import Input.Keyboard;
 
 public class App {
     public static void main(String[] args) {
+        AcceptClient accept = null;
         ConsoleManager.print(Colors.CYAN,"Port: ");
         try {
             int port = Keyboard.getInt();
             
-            new Server(port);
+            accept = new AcceptClient(new ServerSocket(port));
+            accept.start();
+            ConsoleManager.clear();
             ConsoleManager.println(Colors.GREEN, "Started in "+port);
+
+            while(Keyboard.getString().equals("sair")) {
+                accept.kill();
+                System.exit(0);
+            }
         } catch (Exception err) {
             ConsoleManager.println(Colors.RED, "Error in started");
             System.exit(0);
